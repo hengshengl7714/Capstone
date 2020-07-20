1430e4c350abbe7c4d912b40216e747fc33a9081
hengshengl7714 <hengshengl7714@hstat.org>
2020-07-20 17:07:25 +0000

    Finish the static website Version.1

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ class Navbar extends React.Component {
   render() {
     return (
       <div>
-        <nav class="navbar navbar-dark bg-primary navbar-expand-lg navbar-light">
-          <a Class="navbar-brand" href="#">Navbar</a>
+        <nav class="navbar navbar-dark bg-dark navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#">
+            <img src="/pictures/Logo.png" width="70" height="auto" alt="" loading="lazy" />
+             </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -37,6 +39,19 @@ class Navbar extends React.Component {
       </div>
       )
   }
+}
+class Heading extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div class="heading">
+        <h1> Adorable Pet Shop</h1>
+        <h3> Granting your <i>pet</i> a <strong>comfortable</strong> life</h3>
+      </div>
+    )
+  } 
 }
 class Carousel extends React.Component {
   constructor(props){
@@ -84,6 +99,7 @@ class Items extends React.Component {
   render(){
     return (
       <div>
+        <h1 class="heading" id="shop-list">Shop</h1>
         <div class="container-fluid">
           <div class="row">
               <div Class="card col-lg-4 col-md-6 col-sm-12">
@@ -91,7 +107,7 @@ class Items extends React.Component {
                 <div Class="card-body">
                   <h5 Class="card-title">Bear-strip style shirt</h5>
                   <p Class="card-text">Your pet is cute already! A cute suit for your pet to go out will attract more attention.</p>
-                  <h2><span class="badge badge-success">$14.99</span></h2>
+                  <h2><span class="badge badge-success">$12.99</span></h2>
                   <a href="#" Class="btn btn-primary">Add</a>
                 </div>
               </div>
@@ -100,7 +116,7 @@ class Items extends React.Component {
                 <div Class="card-body">
                   <h5 Class="card-title">Black Panda shirt</h5>
                   <p Class="card-text"> Tired of scrambling through stores in search of clothing for your pet? You can buy this product instead! It's great for your pet's comfort and it's warm.  </p>
-                  <h2><span class="badge badge-success">$15.99</span></h2>
+                  <h2><span class="badge badge-success">$8.59</span></h2>
                   <a href="#" Class="btn btn-primary">Add</a>
                 </div>
               </div>
@@ -109,7 +125,7 @@ class Items extends React.Component {
                 <div Class="card-body">
                   <h5 Class="card-title">Hawaiian-style</h5>
                   <p Class="card-text">Hot Summer is here! The Hawaiian style polo shirt definitely suit your pet and let the pet has a cool summer!!</p>
-                  <h2><span class="badge badge-success">$15.99</span></h2>
+                  <h2><span class="badge badge-success">$6.99</span></h2>
                   <a href="#" Class="btn btn-primary">Add</a>
                 </div>
               </div>
@@ -118,7 +134,7 @@ class Items extends React.Component {
                 <div Class="card-body">
                   <h5 Class="card-title">Red-strip hoodie</h5>
                   <p Class="card-text">Comfortable casual sport hoodie for the pets. The cloth is adorable for your lovely sweet pet. </p>
-                  <h2><span class="badge badge-success">$15.99</span></h2>
+                  <h2><span class="badge badge-success">$8.59</span></h2>
                   <a href="#" Class="btn btn-primary">Add</a>
                 </div>
               </div>
@@ -136,8 +152,8 @@ class Items extends React.Component {
                 <div Class="card-body">
                   <h5 Class="card-title">Dountbed for pets</h5>
                   <p Class="card-text">Ultra soft calming bed for your pet. Made by self-warming Cozy Plush, so your pet will have a warming indoor sleeping. </p>
-                  <h2><span class="badge badge-danger"><strike>$34.99</strike></span></h2>
-                  <h2><span class="badge badge-success">$19.99</span></h2>
+                  <h2><span class="badge badge-danger"><strike>$24.99</strike></span></h2>
+                  <h2><span class="badge badge-success">$16.99</span></h2>
                   <a href="#" Class="btn btn-primary">Add</a>
                 </div>
               </div>
@@ -147,7 +163,88 @@ class Items extends React.Component {
     )
   } 
 }
+class ContactUs extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div>
+        <h1 class="heading">Question for US</h1>
+        <form>
+            <div class="form-row">
+              <div class="col">
+                <label> First Name</label>
+                <input type="text" class="form-control"></input>
+              </div>
+              <div class="col">
+                <label> Last Name</label>
+                <input type="text" class="form-control"></input>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="InputEmail1">Email address</label>
+              <input type="email" class="form-control" id="InputEmail1"></input>
+            </div>  
+            <div class="form-group">
+              <label for="ControlSelect">Question relating to our product</label>
+              <select class="form-control" id="ControlSelect">
+                <option> Bear-strip style shirt</option>
+                <option> Black Panda shirt</option>
+                <option> Hawaiian-style</option>
+                <option> Red-strip hoodie</option>
+                <option> Winter Coat</option>
+                <option> Dountbed for pets </option>
+              </select>
+            </div>
+            <div class="form-group">
+            <label for="ControlTextarea">Your Message</label>
+            <textarea class="form-control" id="ControlTextarea" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+class Contact extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div id="contact-heading">
+        <div>
+          <h1 class="heading">Contact</h1>
+        </div>
+        <div>
+          <p id="contact">
+          Address: Adorable Pet Shop, Inc 1234 Pet Road Somewhere, NY 11221<br/>
+          Phone:(555) 555-1234
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
+class Footer extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div id="footer">
+        <p>Copyright © 2020   |   Adorable Pet Shop</p>
+      </div>
+    )
+  }
+}
+
 
 ReactDOM.render(<Navbar />, document.getElementById('root'));
+ReactDOM.render(<Heading />, document.getElementById('heading'));
 ReactDOM.render(<Carousel />, document.getElementById('caro'));
 ReactDOM.render(<Items />, document.getElementById('List'));
+ReactDOM.render(<ContactUs />, document.getElementById('ContactUS'));
+ReactDOM.render(<Contact />, document.getElementById('contact'));
+ReactDOM.render(<Footer />, document.getElementById('footer'));

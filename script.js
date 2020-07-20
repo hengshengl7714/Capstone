88486c79c3cb076b5c826bb4337521f55bca938b
hengshengl7714 <hengshengl7714@hstat.org>
2020-07-20 15:09:14 +0000

    Finish the component of the item

--- script.js
+++ script.js
@@ -49,16 +49,20 @@ class Carousel extends React.Component {
           <li data-target="#carouselIndicators" data-slide-to="0" Class="active"></li>
           <li data-target="#carouselIndicators" data-slide-to="1"></li>
           <li data-target="#carouselIndicators" data-slide-to="2"></li>
+          <li data-target="#carouselIndicators" data-slide-to="3"></li>
         </ol>
         <div Class="carousel-inner">
           <div Class="carousel-item active">
-            <img src="/pictures/Cat1.jpg" Class=" d-block w-100 " alt="Cat1"></img>
+            <img src="/pictures/Carousel-1.JPG" Class=" d-block w-100 " alt="Carousel-1"></img>
           </div>
           <div Class="carousel-item" >
-            <img src="/pictures/Cat2.jpg" Class=" d-block w-100" alt="Cat2"></img>
+            <img src="/pictures/Carousel-2.JPG" Class=" d-block w-100" alt="Carousel-2"></img>
           </div>
           <div Class="carousel-item">
-            <img src="/pictures/Cat3.jpg" Class=" d-block w-100" alt="Cat3"></img>
+            <img src="/pictures/Carousel-3.JPG" Class=" d-block w-100" alt="Carousel-3"></img>
+          </div>
+          <div Class="carousel-item">
+            <img src="/pictures/Carousel-4.JPG" Class=" d-block w-100" alt="Carousel-4"></img>
           </div>
         </div>
         <a Class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
@@ -83,50 +87,57 @@ class Items extends React.Component {
         <div class="container-fluid">
           <div class="row">
               <div Class="card col-lg-4 col-md-6 col-sm-12">
-                <img src="..." Class="card-img-top" alt="..."></img>
+                <img src="/pictures/Bear-shirt.JPG" Class="card-img-top" alt="Bear-shirt"></img>
                 <div Class="card-body">
-                  <h5 Class="card-title">Card title</h5>
-                  <p Class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <h5 Class="card-title">Bear-strip style shirt</h5>
+                  <p Class="card-text">Your pet is cute already! A cute suit for your pet to go out will attract more attention.</p>
+                  <h2><span class="badge badge-success">$14.99</span></h2>
                   <a href="#" Class="btn btn-primary">Add</a>
                 </div>
               </div>
               <div Class="card col-lg-4 col-md-6 col-sm-12">
-                <img src="..." Class="card-img-top" alt="..."></img>
+                <img src="/pictures/Panda-shirt.JPG" Class="card-img-top" alt="Panda-shirt"></img>
                 <div Class="card-body">
-                  <h5 Class="card-title">Card title</h5>
-                  <p Class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <h5 Class="card-title">Black Panda shirt</h5>
+                  <p Class="card-text"> Tired of scrambling through stores in search of clothing for your pet? You can buy this product instead! It's great for your pet's comfort and it's warm.  </p>
+                  <h2><span class="badge badge-success">$15.99</span></h2>
                   <a href="#" Class="btn btn-primary">Add</a>
                 </div>
               </div>
               <div Class="card col-lg-4 col-md-6 col-sm-12">
-                <img src="..." Class="card-img-top" alt="..."></img>
+                <img src="/pictures/Hawaiian-style.JPG" Class="card-img-top" alt="Hawaiian-style"></img>
                 <div Class="card-body">
-                  <h5 Class="card-title">Card title</h5>
-                  <p Class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <h5 Class="card-title">Hawaiian-style</h5>
+                  <p Class="card-text">Hot Summer is here! The Hawaiian style polo shirt definitely suit your pet and let the pet has a cool summer!!</p>
+                  <h2><span class="badge badge-success">$15.99</span></h2>
                   <a href="#" Class="btn btn-primary">Add</a>
                 </div>
               </div>
               <div Class="card col-lg-4 col-md-6 col-sm-12">
-                <img src="..." Class="card-img-top" alt="..."></img>
+                <img src="/pictures/Strip-shirt.JPG" Class="card-img-top" alt="Strip-shirt"></img>
                 <div Class="card-body">
-                  <h5 Class="card-title">Card title</h5>
-                  <p Class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <h5 Class="card-title">Red-strip hoodie</h5>
+                  <p Class="card-text">Comfortable casual sport hoodie for the pets. The cloth is adorable for your lovely sweet pet. </p>
+                  <h2><span class="badge badge-success">$15.99</span></h2>
                   <a href="#" Class="btn btn-primary">Add</a>
                 </div>
               </div>
               <div Class="card col-lg-4 col-md-6 col-sm-12">
-                <img src="..." Class="card-img-top" alt="..."></img>
+                <img src="/pictures/Dog-coat.JPG" Class="card-img-top" alt="Dog-coat"></img>
                 <div Class="card-body">
-                  <h5 Class="card-title">Card title</h5>
-                  <p Class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <h5 Class="card-title">Winter Coat</h5>
+                  <p Class="card-text">Warm coat that gives your pet a warmful winter. Let your pet run around without fearing the coldness.</p>
+                  <h2><span class="badge badge-success">$15.99</span></h2>
                   <a href="#" Class="btn btn-primary">Add</a>
                 </div>
               </div>
               <div Class="card col-lg-4 col-md-6 col-sm-12">
-                <img src="..." Class="card-img-top" alt="..."></img>
+                <img src="/pictures/Dog-nest.JPG" Class="card-img-top" alt="Dog-nest"></img>
                 <div Class="card-body">
-                  <h5 Class="card-title">Card title</h5>
-                  <p Class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <h5 Class="card-title">Dountbed for pets</h5>
+                  <p Class="card-text">Ultra soft calming bed for your pet. Made by self-warming Cozy Plush, so your pet will have a warming indoor sleeping. </p>
+                  <h2><span class="badge badge-danger"><strike>$34.99</strike></span></h2>
+                  <h2><span class="badge badge-success">$19.99</span></h2>
                   <a href="#" Class="btn btn-primary">Add</a>
                 </div>
               </div>
@@ -136,6 +147,7 @@ class Items extends React.Component {
     )
   } 
 }
+
 ReactDOM.render(<Navbar />, document.getElementById('root'));
 ReactDOM.render(<Carousel />, document.getElementById('caro'));
 ReactDOM.render(<Items />, document.getElementById('List'));
